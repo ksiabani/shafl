fd962874a85172217b007ace11c8962b41231b44
Kostas Siabanis <kostas.siabanis@gmail.com>
2018-09-24 23:06:47 +0300

    Add mongoose, use dotenv with env file, add configuration service

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ApiModule } from 'api/api.module';
+import { ConfigService } from './config/config.service';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [ApiModule],
+  imports: [MongooseModule.forRoot(new ConfigService('.env').get('MONGODB_URI')), ApiModule, ConfigModule],
 })
 export class AppModule {}
